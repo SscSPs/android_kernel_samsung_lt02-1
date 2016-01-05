57f96c06ce6993eeb58a4bf5edbdcc56e7cbdaae
Taylor Smock <smocktaylor@gmail.com>
2016-01-05 13:54:05 -0700

    area-include-net-bluetooth.diff
    	From upstream analysis tools (modified)

--- include/net/bluetooth/l2cap_mgmt.h
+++ include/net/bluetooth/l2cap_mgmt.h
@@ -23,11 +23,7 @@
    COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS, RELATING TO USE OF THIS
    SOFTWARE IS DISCLAIMED.
 */
-#ifdef CONFIG_BT_MGMT
-#include "l2cap_mgmt.h"
-#elif defined(CONFIG_BT_TIZEN)
-#include "tizen/l2cap.h"
-#else
+
 #ifndef __L2CAP_H
 #define __L2CAP_H
 
@@ -43,8 +39,14 @@
 #define L2CAP_DEFAULT_ACK_TO		200
 #define L2CAP_LE_DEFAULT_MTU		23
 
+#define L2CAP_DISC_TIMEOUT             (100)
+#define L2CAP_DISC_REJ_TIMEOUT         (5000)  /*  5 seconds */
+#define L2CAP_ENC_TIMEOUT              (5000)  /*  5 seconds */
 #define L2CAP_CONN_TIMEOUT	(40000) /* 40 seconds */
 #define L2CAP_INFO_TIMEOUT	(4000)  /*  4 seconds */
+/* SSBT :: KJH * 15sec -> 5sec */
+#define L2CAP_CONN_LE_TIMEOUT	(5000) /* 5 seconds */
+
 
 /* L2CAP socket address */
 struct sockaddr_l2 {
@@ -108,18 +110,18 @@ struct l2cap_conninfo {
 #define L2CAP_FCS_CRC16		0x01
 
 /* L2CAP Control Field bit masks */
-#define L2CAP_CTRL_SAR			0xC000
-#define L2CAP_CTRL_REQSEQ		0x3F00
-#define L2CAP_CTRL_TXSEQ		0x007E
-#define L2CAP_CTRL_RETRANS		0x0080
-#define L2CAP_CTRL_FINAL		0x0080
-#define L2CAP_CTRL_POLL			0x0010
+#define L2CAP_CTRL_SAR               0xC000
+#define L2CAP_CTRL_REQSEQ            0x3F00
+#define L2CAP_CTRL_TXSEQ             0x007E
+#define L2CAP_CTRL_RETRANS           0x0080
+#define L2CAP_CTRL_FINAL             0x0080
+#define L2CAP_CTRL_POLL              0x0010
 #define L2CAP_CTRL_SUPERVISE         0x000C
-#define L2CAP_CTRL_FRAME_TYPE		0x0001 /* I- or S-Frame */
+#define L2CAP_CTRL_FRAME_TYPE        0x0001 /* I- or S-Frame */
 
-#define L2CAP_CTRL_TXSEQ_SHIFT		1
-#define L2CAP_CTRL_REQSEQ_SHIFT		8
-#define L2CAP_CTRL_SAR_SHIFT		14
+#define L2CAP_CTRL_TXSEQ_SHIFT      1
+#define L2CAP_CTRL_REQSEQ_SHIFT     8
+#define L2CAP_CTRL_SAR_SHIFT       14
 
 /* L2CAP Supervisory Function */
 #define L2CAP_SUPER_RCV_READY           0x0000
@@ -172,14 +174,14 @@ struct l2cap_conn_rsp {
 #define L2CAP_CID_DYN_START	0x0040
 #define L2CAP_CID_DYN_END	0xffff
 
-/* connect result */
+/* connect/create channel results */
 #define L2CAP_CR_SUCCESS	0x0000
 #define L2CAP_CR_PEND		0x0001
 #define L2CAP_CR_BAD_PSM	0x0002
 #define L2CAP_CR_SEC_BLOCK	0x0003
 #define L2CAP_CR_NO_MEM		0x0004
 
-/* connect status */
+/* connect/create channel status */
 #define L2CAP_CS_NO_INFO	0x0000
 #define L2CAP_CS_AUTHEN_PEND	0x0001
 #define L2CAP_CS_AUTHOR_PEND	0x0002
@@ -256,13 +258,13 @@ struct l2cap_info_rsp {
 } __packed;
 
 /* info type */
-#define L2CAP_IT_CL_MTU		0x0001
-#define L2CAP_IT_FEAT_MASK	0x0002
-#define L2CAP_IT_FIXED_CHAN	0x0003
+#define L2CAP_IT_CL_MTU     0x0001
+#define L2CAP_IT_FEAT_MASK  0x0002
+#define L2CAP_IT_FIXED_CHAN 0x0003
 
 /* info result */
-#define L2CAP_IR_SUCCESS	0x0000
-#define L2CAP_IR_NOTSUPP	0x0001
+#define L2CAP_IR_SUCCESS    0x0000
+#define L2CAP_IR_NOTSUPP    0x0001
 
 struct l2cap_conn_param_update_req {
 	__le16      min;
@@ -357,15 +359,15 @@ struct l2cap_chan {
 	struct sk_buff_head	srej_q;
 	struct list_head	srej_l;
 
-	struct list_head	list;
-	struct list_head	global_l;
+	struct list_head list;
+	struct list_head global_l;
 
-	void			*data;
-	struct l2cap_ops	*ops;
+	void		*data;
+	struct l2cap_ops *ops;
 };
 
 struct l2cap_ops {
-	char			*name;
+	char		*name;
 
 	struct l2cap_chan	*(*new_connection) (void *data);
 	int			(*recv) (void *data, struct sk_buff *skb);
@@ -373,28 +375,47 @@ struct l2cap_ops {
 	void			(*state_change) (void *data, int state);
 };
 
+//+++ MRVL_LOCAL
+#define MRVL_LOCAL_SCHDL_ACL
+
+#ifdef MRVL_LOCAL_SCHDL_ACL
+struct l2cap_pend_conn_req {
+    struct l2cap_cmd_hdr cmd;
+    struct l2cap_conn_req conn_req;
+};
+#endif
+//--- MRVL_LOCAL
+
 struct l2cap_conn {
-	struct hci_conn		*hcon;
+	struct hci_conn	*hcon;
 
-	bdaddr_t		*dst;
-	bdaddr_t		*src;
+	bdaddr_t	*dst;
+	bdaddr_t	*src;
 
-	unsigned int		mtu;
+	unsigned int	mtu;
 
-	__u32			feat_mask;
+	__u32		feat_mask;
 
-	__u8			info_state;
-	__u8			info_ident;
+	__u8		info_state;
+	__u8		info_ident;
 
 	struct timer_list info_timer;
 
-	spinlock_t		lock;
+//+++ MRVL_LOCAL
+#ifdef MRVL_LOCAL_SCHDL_ACL
+    struct timer_list encrypt_timer;
 
-	struct sk_buff		*rx_skb;
-	__u32			rx_len;
-	__u8			tx_ident;
+    struct l2cap_pend_conn_req *p_req;
+#endif
+//--- MRVL_LOCAL
 
-	__u8			disc_reason;
+	spinlock_t	lock;
+
+	struct sk_buff *rx_skb;
+	__u32		rx_len;
+	__u8		tx_ident;
+
+	__u8		disc_reason;
 
 	__u8		preq[7]; /* SMP Pairing Request */
 	__u8		prsp[7]; /* SMP Pairing Response */
@@ -405,7 +426,9 @@ struct l2cap_conn {
 
 	struct timer_list security_timer;
 
-	struct list_head	chan_l;
+	struct smp_chan *smp_chan;
+
+	struct list_head chan_l;
 	rwlock_t	chan_lock;
 };
 
@@ -421,9 +444,9 @@ struct l2cap_conn {
 #define l2cap_pi(sk) ((struct l2cap_pinfo *) sk)
 
 struct l2cap_pinfo {
-	struct bt_sock		bt;
+	struct bt_sock	bt;
 	struct l2cap_chan	*chan;
-	struct sk_buff		*rx_busy_skb;
+	struct sk_buff	*rx_busy_skb;
 };
 
 enum {
@@ -483,7 +506,7 @@ static inline int l2cap_tx_window_full(struct l2cap_chan *ch)
 #define __is_sframe(ctrl)	((ctrl) & L2CAP_CTRL_FRAME_TYPE)
 #define __is_sar_start(ctrl)	(((ctrl) & L2CAP_CTRL_SAR) == L2CAP_SDU_START)
 
-extern int disable_ertm;
+extern bool disable_ertm;
 
 int l2cap_init_sockets(void);
 void l2cap_cleanup_sockets(void);
@@ -502,5 +525,3 @@ int l2cap_chan_send(struct l2cap_chan *chan, struct msghdr *msg, size_t len);
 void l2cap_chan_busy(struct l2cap_chan *chan, int busy);
 
 #endif /* __L2CAP_H */
-
-#endif /* BT_MGMT */
